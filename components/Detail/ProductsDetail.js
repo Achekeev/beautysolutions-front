fc9e6a29f1d4e68df358416bac3b54dbe5132858
WIN-2QJDOS8CRCD\user <baxxromm@gmail.com>
2023-08-08 13:30:29 +0600

    up-date

--- components/Detail/ProductsDetail.js
+++ components/Detail/ProductsDetail.js
@@ -7,112 +7,109 @@ import { addBasket, basketAction } from "../../redux/action/basketAction";
 import { useTranslation } from "react-i18next";
 
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className + " next-button-slick"}
-            style={{ ...style, display: "block", background: "transparent" }}
-            onClick={onClick}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className + " next-button-slick"}
+      style={{ ...style, display: "block", background: "transparent" }}
+      onClick={onClick}
+    />
+  );
 }
 
 function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className + " prev-button-slick"}
-            style={{ ...style, display: "block", background: "transparent" }}
-            onClick={onClick}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className + " prev-button-slick"}
+      style={{ ...style, display: "block", background: "transparent" }}
+      onClick={onClick}
+    />
+  );
 }
 
 const ProductsDetail = ({ productsDetail }) => {
-    const {t} = useTranslation()
+  const { t } = useTranslation();
 
-    const { basket } = useSelector((state) => state.basket);
-    const dispatch = useDispatch();
+  const { basket } = useSelector((state) => state.basket);
+  const dispatch = useDispatch();
 
-    function onClickButton(e) {
-        e.preventDefault();
-        addBasket(productsDetail.id);
-        dispatch(basketAction());
-    }
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-    };
-    return (
-        <div className="container" style={{'marginTop' : '6em', 'marginBottom' : '4em'}}>
-            <div className={styles.content}>
-                <div className={styles.slider}>
-                    <Slider {...settings}>
-
-
-                            <div>
-                                      <Image
-                                        loader={() => '/placeholder-image.png'}
-                                        src={productsDetail.images? productsDetail.images[0].image: '/placeholder-image.png'}
-                                        placeholder="/placeholder-image.png"
-                                        objectFit={"contain"}
-                                        alt=""
-                                        width={600}
-                                        height={600}
-                                        unoptimized
-                                    />
-                           </div>
-                    </Slider>
-                </div>
-                <div>
-                    <div className={styles.name}>
-                        <h2>{productsDetail.name}</h2>
-
-                        {productsDetail.sale && (
-                            <div className={styles.sale}>
-                                <div>{productsDetail.discount}%</div>
-                            </div>
-                        )}
-                    </div>
-
-                   <div style={{'marginBottom' : '1.5em'}}><strong>{t('info')}:</strong> {productsDetail.description}</div>
-                    <div className={styles.btn_card_price}>
-                        <div className={styles.price}>
-                            <strong>{t('price')}:</strong>{" "}
-                            <span
-                                className={
-                                    productsDetail.sale ? styles.dis_price : ""
-                                }
-                            >
-                                {productsDetail.price &&
-                                    productsDetail.price.replace(
-                                        /\..*/,
-                                        ""
-                                    )}{" "}
-                                $
-                            </span>{" "}
-                            {productsDetail.sale && (
-                                <span>{productsDetail.discount_price} $</span>
-                            )}
-                        </div>
-                        <div
-                            className={styles.best_card_btn}
-                            onClick={onClickButton}
-                        >
-                            {basket.indexOf(productsDetail.id) !== -1
-                                ? t('removeFromBasket')
-                                : t('inBasket')}
-                        </div>
-                    </div>
-                </div>
+  function onClickButton(e) {
+    e.preventDefault();
+    addBasket(productsDetail.id);
+    dispatch(basketAction());
+  }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <div
+      className="container"
+      style={{ marginTop: "6em", marginBottom: "4em" }}
+    >
+      <div className={styles.content}>
+        <div className={styles.slider}>
+          <Slider {...settings}>
+            <div>
+              <Image
+                loader={() => "/placeholder-image.png"}
+                src={
+                  productsDetail.images
+                    ? productsDetail.images[0].image
+                    : "/placeholder-image.png"
+                }
+                placeholder="/placeholder-image.png"
+                objectFit={"contain"}
+                alt=""
+                width={600}
+                height={600}
+                unoptimized
+              />
             </div>
+          </Slider>
         </div>
-    );
+        <div>
+          <div className={styles.name}>
+            <h2>{productsDetail.name}</h2>
+
+            {productsDetail.sale && (
+              <div className={styles.sale}>
+                <div>{productsDetail.discount}%</div>
+              </div>
+            )}
+          </div>
+
+          <div style={{ marginBottom: "1.5em" }}>
+            <strong>{t("info")}:</strong> {productsDetail.description}
+          </div>
+          <div className={styles.btn_card_price}>
+            <div className={styles.price}>
+              <strong>{t("price")}:</strong>
+              <span className={productsDetail.sale ? styles.dis_price : ""}>
+                {productsDetail.price &&
+                  productsDetail.price.replace(/\..*/, "")}{" "}
+                ₽
+              </span>{" "}
+              {productsDetail.sale && (
+                <span>{productsDetail.discount_price} ₽</span>
+              )}
+            </div>
+            <div className={styles.best_card_btn} onClick={onClickButton}>
+              {basket.indexOf(productsDetail.id) !== -1
+                ? t("removeFromBasket")
+                : t("inBasket")}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsDetail;
--- components/Looked/Looked.jsx
+++ components/Looked/Looked.jsx
@@ -1,51 +1,44 @@
-import React from 'react';
-import styles from './Looked.module.css';
+import React from "react";
+import styles from "./Looked.module.css";
 
-import Image from 'next/image';
-import LoadMore from '../LoadMore/LoadMore';
-import { useTranslation } from 'react-i18next';
+import Image from "next/image";
+import LoadMore from "../LoadMore/LoadMore";
+import { useTranslation } from "react-i18next";
 
 const BestCard = (props) => {
-    const {t} = useTranslation()
+  const { t } = useTranslation();
 
-    return (
-        <div className={styles.best_card}>
-            <div className={styles.image_wrapper}>
-                <Image
-                    src='/kreslo.png'
-                    alt='sofa img'
-                    width={154}
-                    height={156}
-                />
-            </div>
-            <div className={styles.line}></div>
-            <div className={styles.best_center}>
-                <div className={styles.best_card_name}>{t('chairSwoon')}</div>
-                <div className={styles.best_card_descr}>
-                    {t('fishText')}
-                </div>
-                <div className={styles.best_card_price}>15 040 $</div>
-            </div>
-            <div className={styles.best_card_btn}>{t('inBasket')}</div>
-        </div>
-    );
+  return (
+    <div className={styles.best_card}>
+      <div className={styles.image_wrapper}>
+        <Image src="/kreslo.png" alt="sofa img" width={154} height={156} />
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.best_center}>
+        <div className={styles.best_card_name}>{t("chairSwoon")}</div>
+        <div className={styles.best_card_descr}>{t("fishText")}</div>
+        <div className={styles.best_card_price}>15 040 ₽</div>
+      </div>
+      <div className={styles.best_card_btn}>{t("inBasket")}</div>
+    </div>
+  );
 };
 
 const Look = () => {
-    return (
-        <div className='gray_bg'>
-            <div className={styles.best_container}>
-                <h2>{t('youWatchingLater')}</h2>
-                <div className={styles.best_card_wrapper}>
-                    <BestCard />
-                    <BestCard />
-                    <BestCard />
-                    <BestCard />
-                </div>
-            </div>
-            <LoadMore />
+  return (
+    <div className="gray_bg">
+      <div className={styles.best_container}>
+        <h2>{t("youWatchingLater")}</h2>
+        <div className={styles.best_card_wrapper}>
+          <BestCard />
+          <BestCard />
+          <BestCard />
+          <BestCard />
         </div>
-    );
+      </div>
+      <LoadMore />
+    </div>
+  );
 };
 
 export default Look;
